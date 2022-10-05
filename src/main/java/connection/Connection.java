8dd5ad00a553402e4b4d9564c5ab662fc2009e68
xuanhieuvnhd <104875522+xuanhieuvnhd@users.noreply.github.com>
2022-10-05 17:08:10 +0700

    Update Connection.java

--- src/main/java/connection/Connection.java
+++ src/main/java/connection/Connection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connection {
-    private static final String URL = "jdbc:mysql://localhost:3306/webbansach";
+    private static final String URL = "jdbc:mysql://localhost:3306/";
     private static final String USER = "root";
     private static final String PASSWORD = "761321";
 
