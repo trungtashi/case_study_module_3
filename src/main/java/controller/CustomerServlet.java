011d06a69f9a7c05495b9aebe87377cf4dde2343
xuanhieuvnhd <104875522+xuanhieuvnhd@users.noreply.github.com>
2022-10-06 15:12:35 +0700

    Add files via upload

---
+++ src/main/java/controller/CustomerServlet.java
@@ -0,0 +1,163 @@
+package controller;
+
+
+import dao.CustomerDAO;
+import model.Customer;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "CustomerServlet", urlPatterns = "/customers")
+public class CustomerServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private CustomerDAO customerDAO;
+
+    public void init() {
+        customerDAO = new CustomerDAO();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "create":
+                    insertCustomerr(request, response);
+                    break;
+                case "edit":
+                    updateCustomer(request, response);
+                    break;
+                case "search":
+                    searchCustomerByName(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+
+        try {
+            switch (action) {
+                case "create":
+                    showNewForm(request, response);
+                    break;
+                case "edit":
+                    showEditForm(request, response);
+                    break;
+                case "delete":
+                    deleteCustomer(request, response);
+                    break;
+                case "sort":
+                    sortCustomer(request, response);
+                default:
+                    listCustomer(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void listCustomer(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<Customer> customers= customerDAO.selectAllCustomer();
+        request.setAttribute("customer", customers);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/create.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Customer existingCustomer = customerDAO.selectCustomer(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/edit.jsp");
+        request.setAttribute("customer", existingCustomer);
+        dispatcher.forward(request, response);
+
+    }
+
+    private void insertCustomerr(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        String customerName = request.getParameter("customerName");
+        int customerAge = Integer.parseInt(request.getParameter("customerAge"));
+        String customerAddress = request.getParameter("customerAddress");
+        String customerPhone = request.getParameter("customerPhone");
+        String customerEmail = request.getParameter("customerEmail");
+        String account = request.getParameter("acount");
+        String password = request.getParameter("password");
+        Customer newCustomer = new Customer(customerName,customerAge,customerAddress,customerPhone,customerEmail,account,password);
+        customerDAO.insertCustomer(newCustomer);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/create.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void updateCustomer(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        int customerId = Integer.parseInt(request.getParameter("customerId"));
+        String customerName = request.getParameter("customerName");
+        int customerAge = Integer.parseInt(request.getParameter("customerAge"));
+        String customerAddress = request.getParameter("customerAddress");
+        String customerPhone = request.getParameter("customerPhone");
+        String customerEmail = request.getParameter("customerEmail");
+        String account = request.getParameter("acount");
+        String password = request.getParameter("password");
+        Customer newCustomer = new Customer(customerName,customerAge,customerAddress,customerPhone,customerEmail,account,password);
+        customerDAO.updateCustomer(newCustomer);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/edit.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void deleteCustomer(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        customerDAO.deleteCustomer(id);
+
+        List<Customer> customers = customerDAO.selectAllCustomer();
+        request.setAttribute("customer", customers);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void searchCustomerByName(HttpServletRequest request, HttpServletResponse response) throws SQLException,IOException,ServletException {
+        String customerName = request.getParameter("input");
+        List<Customer> customers = customerDAO.searchCustomerByName(customerName);
+        request.setAttribute("customer", customers);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/customer/list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showSearchForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/searchbyname.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void sortCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Customer> customers = customerDAO.sortByName();
+        request.setAttribute("customer", customers);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/sortbyname.jsp");
+        dispatcher.forward(request, response);
+    }
+}
