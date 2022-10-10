2382e55a306d89a773b59d3c9932817fff80e2d1
xuanhieuvnhd <104875522+xuanhieuvnhd@users.noreply.github.com>
2022-10-10 15:23:33 +0700

    Add files via upload

---
+++ src/main/java/dao/ICategoryDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import model.Category;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ICategoryDAO {
+    public void insertCategory(Category category) throws SQLException;
+
+    public Category selectCategory(int id);
+
+    public List<Category> selectAllCategory();
+
+    public boolean deleteCategory(int id) throws SQLException;
+
+    public boolean updateCategory(Category category) throws SQLException;
+}
